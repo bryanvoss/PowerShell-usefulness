b852702981df82344ec28e03384043fe810fc268
bryanvoss <bvoss@vosswerx.com>
2022-02-08 13:27:18 -0600

    Fixed light theme message colors.

--- Set-LightTheme.ps1
+++ Set-LightTheme.ps1
@@ -1,9 +1,9 @@
 Write-Host -NoNewline "Setting SYSTEM theme to: "
-Write-Host -BackgroundColor Black -ForegroundColor White "Light"
+Write-Host -BackgroundColor White -ForegroundColor Black "Light"
 
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 1
 
 Write-Host -NoNewline "Setting APPS theme to: "
-Write-Host -BackgroundColor Black -ForegroundColor White "Light"
+Write-Host -BackgroundColor White -ForegroundColor Black "Light"
 
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 1
